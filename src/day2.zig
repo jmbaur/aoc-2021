e0684c31f84f7c40d686cb1bb046ff758b46be74
Jared Baur <jaredbaur@fastmail.com>
2021-12-03 14:55:11 -0800

    Day2 Part2

--- src/day2.zig
+++ src/day2.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const print = std.debug.print;
 
 const ascii_newline = 10;
 const ascii_space = 32;
@@ -48,16 +47,67 @@ pub fn submarine_position(input: []const u8) i32 {
     return horizontal * depth;
 }
 
-test "submarine_position" {
-    const input =
-        \\forward 5
-        \\down 5
-        \\forward 8
-        \\up 3
-        \\down 8
-        \\forward 2
-    ;
+pub fn submarine_position_with_aim(input: []const u8) i32 {
+    var depth: i32 = 0;
+    var horizontal: i32 = 0;
+    var aim: i32 = 0;
 
+    var line_iter = std.mem.split(u8, input, "\n");
+
+    while (true) {
+        const m_line = line_iter.next();
+        if (m_line) |line| {
+            var command_iter = std.mem.split(u8, line, " ");
+            const m_direction = command_iter.next();
+            if (m_direction) |direction| {
+                var unit: i32 = 0;
+
+                const m_unit = command_iter.next();
+                if (m_unit) |str_unit| {
+                    unit = std.fmt.parseInt(i32, str_unit, 0) catch continue;
+                } else {
+                    continue;
+                }
+
+                switch (direction[0]) {
+                    'f' => {
+                        horizontal += unit;
+                        depth += aim * unit;
+                    },
+                    'u' => {
+                        aim -= unit;
+                    },
+                    'd' => {
+                        aim += unit;
+                    },
+                    else => {},
+                }
+            } else {
+                continue;
+            }
+        } else {
+            break;
+        }
+    }
+
+    return horizontal * depth;
+}
+
+const test_input =
+    \\forward 5
+    \\down 5
+    \\forward 8
+    \\up 3
+    \\down 8
+    \\forward 2
+;
+
+test "submarine_position" {
     const expect: i32 = 150;
-    try std.testing.expectEqual(expect, submarine_position(input.*[0..]));
+    try std.testing.expectEqual(expect, submarine_position(test_input.*[0..]));
+}
+
+test "submarine_position_with_aim" {
+    const expect: i32 = 900;
+    try std.testing.expectEqual(expect, submarine_position_with_aim(test_input.*[0..]));
 }
--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,8 @@ const print = std.debug.print;
 const day1 = @import("./day1.zig");
 const day2 = @import("./day2.zig");
 
-const day1_part1_input = @embedFile("../inputs/day1/part1.txt");
-const day1_part2_input = @embedFile("../inputs/day1/part2.txt");
-const day2_part1_input = @embedFile("../inputs/day2/part1.txt");
+const day1_input = @embedFile("../inputs/day1/input.txt");
+const day2_input = @embedFile("../inputs/day2/input.txt");
 
 fn get_number_list(a: *std.mem.Allocator, r: anytype) anyerror!std.ArrayList(i32) {
     var msg_buf: [4096]u8 = undefined;
@@ -28,17 +27,19 @@ pub fn main() anyerror!void {
     var allocator = &arena.allocator;
 
     // Day 1
-    const day1_part1_list = try get_number_list(allocator, std.io.fixedBufferStream(day1_part1_input).reader());
+    const day1_part1_list = try get_number_list(allocator, std.io.fixedBufferStream(day1_input).reader());
     const day1_part1_sol = day1.num_of_increases(day1_part1_list.items);
     print("day1 part1 solution: {d}\n", .{day1_part1_sol});
     day1_part1_list.deinit();
 
-    const day1_part2_list = try get_number_list(allocator, std.io.fixedBufferStream(day1_part2_input).reader());
+    const day1_part2_list = try get_number_list(allocator, std.io.fixedBufferStream(day1_input).reader());
     const day1_part2_sol = day1.three_window_num_of_increases(day1_part2_list.items);
     print("day1 part2 solution: {d}\n", .{day1_part2_sol});
     day1_part2_list.deinit();
 
     // Day 2
-    const day2_part1_sol = day2.submarine_position(day2_part1_input);
+    const day2_part1_sol = day2.submarine_position(day2_input);
     print("day2 part1 solution: {d}\n", .{day2_part1_sol});
+    const day2_part2_sol = day2.submarine_position_with_aim(day2_input);
+    print("day2 part2 solution: {d}\n", .{day2_part2_sol});
 }
